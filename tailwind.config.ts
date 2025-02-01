444170adb7539afee17f798d830267cab7fc8df2
Magda <wozna.magda@gmail.com>
2025-02-01 11:53:55 +0100

    Add missing config, remove redundant file

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -4,15 +4,11 @@ export default {
   content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/sections/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-    extend: {
-      colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
-      },
-    },
+    extend: {},
   },
   plugins: [],
 } satisfies Config;
