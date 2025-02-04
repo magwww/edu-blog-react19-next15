791e38c5ab6f55d750e3c50e2fc03ef0930ab910
Magda <wozna.magda@gmail.com>
2025-02-04 15:08:09 +0100

    Add posts page error

---
+++ src/app/error.tsx
@@ -0,0 +1,37 @@
+"use client";
+
+import { useRouter } from "next/navigation";
+import { startTransition, useEffect } from "react";
+
+export default function ErrorBoundary({
+  error,
+  reset,
+}: {
+  error: Error & { digest?: string };
+  reset: () => void;
+}) {
+  const router = useRouter();
+  const reload = () => {
+    startTransition(() => {
+      router.refresh();
+      reset();
+    });
+  };
+
+  useEffect(() => {
+    // Log the error to an error reporting service
+    console.error("Error in post/id:", error);
+  }, [error]);
+
+  return (
+    <div className="flex flex-col items-center gap-4 lg:px-24 lg:py-16 p-4">
+      <h1>Nie udało się pobrać postów</h1>
+      <button
+        className="group flex items-center gap-2 px-6 py-2 border rounded-xl"
+        onClick={() => reload()}
+      >
+        Spróbuj ponownie
+      </button>
+    </div>
+  );
+}
